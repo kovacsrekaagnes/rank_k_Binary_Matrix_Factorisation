11a14c7b71dc5d4073f08dbac305d91c8f478ade
Reka <reka.agnes@gmail.com>
2022-08-23 17:32:51 +0100

    added multiple thresholds to experiment_nmf.py

--- experiment_nmf.py
+++ experiment_nmf.py
@@ -27,7 +27,7 @@ def from_NMF_to_BMF(A_NMF,B_NMF, threshold=0.5):
 
 test_set = ['zoo', 'heart','lymp','apb']#,'tumor_w_missing','hepatitis_w_missing', 'audio_w_missing', 'votes_w_missing']
 ks = [2, 5, 10]
-df_index = pd.MultiIndex.from_product([['error', 'time'], ['k=%s' %k for k in ks]])
+df_index = pd.MultiIndex.from_product([['error', 'time', 'threshold'], ['k=%s' %k for k in ks]])
 df = pd.DataFrame(index=df_index, columns=test_set)
 
 for name in test_set:
@@ -35,17 +35,26 @@ for name in test_set:
     (n,m) = X.shape
     for k in ks:
 
+        #initialise a large error value, just to be able to compare later and record best
+        df.loc[('error', 'k=%s' % k), name] = n*m
+
         start = time.time()
         A_NMF, B_NMF, _ = non_negative_factorization(X, n_components=k)
         end = time.time()
-
-        A, B = from_NMF_to_BMF(A_NMF, B_NMF)
         t = end - start
 
-        error = np.sum(np.abs(X - boolean_matrix_product(A, B)))
+        for threshold in np.arange(0.1, 1, 0.1):
+            start = time.time()
+            A, B = from_NMF_to_BMF(A_NMF, B_NMF, threshold)
+            end = time.time()
 
-        df.loc[('error', 'k=%s' % k), name] = error
-        df.loc[('time', 'k=%s' % k), name] = t
+            error = np.sum(np.abs(X - boolean_matrix_product(A, B)))
+
+            if error < df.loc[('error', 'k=%s' % k), name]:
+
+                df.loc[('error', 'k=%s' % k), name] = error
+                df.loc[('time', 'k=%s' % k), name] = t + end - start
+                df.loc[('threshold', 'k=%s' % k), name] = threshold
 
         df.to_csv('./experiments/real_data/nmf_scaled.csv')
 
