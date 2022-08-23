46d3a60dabc620b384b90cd013ddb8c669091fd3
Reka <reka.agnes@gmail.com>
2022-08-23 19:26:40 +0100

    added threshold and random seed selection to matlab NMF completion experiments

--- nmfv1_4.m
+++ nmfv1_4.m
@@ -4,8 +4,8 @@ clear
 data_folder_path = 'data_folder'
 
 names = {'tumor_w_missing','hepatitis_w_missing','audio_w_missing','votes_w_missing'};
-results = [];
 ks = [2, 5, 10];
+results = zeros(size(names,2),3*size(ks,2));
 
 for i = 1:length(names)
     
@@ -15,20 +15,45 @@ for i = 1:length(names)
     for j = 1:3
         
         k = ks(j);
+        % a big initial value of error
+        results(i, j) = prod(size(X))
         
-        % Directly call the weighted NMF algorithm to obtain the factor matrices.
-        [A_nmf, B_nmf] = wnmfrule(X,k);
+        for r = 0:10
+            rng(r)
+            % Directly call the weighted NMF algorithm to obtain the factor matrices.
+            [A_nmf, B_nmf] = wnmfrule(X,k);
 
-        A = A_nmf > 0.5;
-        B = B_nmf > 0.5;
+            for threshold = 0.1:0.1:0.9
+                [A, B] = NMF_to_BMF(A_nmf, B_nmf, threshold);
 
-        Z = A*B >= 1;
+                Z = A*B >= 1;
 
-        results(i, j) = sum(sum(abs(X - Z)==1));
+                error = sum(sum(abs(X - Z)==1));
+                if error < results(i, j)
+                    results(i, j) = error;
+                    results(i, j+size(ks,2)) = threshold;
+                    results(i, j+2*size(ks,2)) = r;
+                end
+            end
+        end
     end
-        
 end
         
 
 csvwrite('nmf_completion.csv',results)
 
+function [A, B] = NMF_to_BMF(A_nmf, B_nmf, threshold)
+if nargin <=2
+    threshold = 0.5;
+end
+kk = size(A_nmf,2);
+for p=1:kk
+    maxAp = max(A_nmf(:,p));
+    maxBp = max(B_nmf(p,:));
+    A_nmf(:,p) = A_nmf(:,p)/ sqrt(maxAp) * sqrt(maxBp);
+    B_nmf(:,p) = B_nmf(:,p)/ sqrt(maxBp) * sqrt(maxAp);
+end
+A = (A_nmf > threshold);
+B = (B_nmf > threshold);
+    
+end
