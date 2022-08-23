ce8b456fbde5a8ae6f2efaab306d019622f8fb7c
Reka <reka.agnes@gmail.com>
2022-08-23 17:10:14 +0100

    added scaling to NMF factors before thresholding to get BMF

--- experiment_nmf.py
+++ experiment_nmf.py
@@ -5,6 +5,26 @@ import pandas as pd
 import time
 EPS = np.finfo(np.float32).eps
 
+#convert NMF factorisation to BMF, and first scale NMF factors
+def from_NMF_to_BMF(A_NMF,B_NMF, threshold=0.5):
+
+    k = A_NMF.shape[1]
+
+    #scale NMF factors
+    for p in range(k):
+        max_Ap = A_NMF[:, p].max()
+        max_Bp = B_NMF[p, :].max()
+        A_NMF[:, p] = A_NMF[:, p] / np.sqrt(max_Ap) * np.sqrt(max_Bp)
+        B_NMF[p, :] = B_NMF[p, :] / np.sqrt(max_Bp) * np.sqrt(max_Ap)
+
+    #threshold NMF to get BMF
+    A_BMF = np.zeros(A_NMF.shape, dtype=int)
+    A_BMF[A_NMF > threshold] = 1
+    B_BMF = np.zeros(B_NMF.shape, dtype=int)
+    B_BMF[B_NMF > threshold] = 1
+
+    return A_BMF, B_BMF
+
 test_set = ['zoo', 'heart','lymp','apb']#,'tumor_w_missing','hepatitis_w_missing', 'audio_w_missing', 'votes_w_missing']
 ks = [2, 5, 10]
 df_index = pd.MultiIndex.from_product([['error', 'time'], ['k=%s' %k for k in ks]])
@@ -16,24 +36,18 @@ for name in test_set:
     for k in ks:
 
         start = time.time()
-        out = non_negative_factorization(X, n_components=k)
+        A_NMF, B_NMF, _ = non_negative_factorization(X, n_components=k)
         end = time.time()
 
-        A_NMF = out[0]
-        B_NMF = out[1]
-
-        A = np.zeros([n, k], dtype=int)
-        A[A_NMF > 0.5] = 1
-        B = np.zeros([k, m], dtype=int)
-        B[B_NMF > 0.5] = 1
-
+        A, B = from_NMF_to_BMF(A_NMF, B_NMF)
         t = end - start
+
         error = np.sum(np.abs(X - boolean_matrix_product(A, B)))
 
         df.loc[('error', 'k=%s' % k), name] = error
         df.loc[('time', 'k=%s' % k), name] = t
 
-        df.to_csv('./experiments/real_data/nmf.csv')
+        df.to_csv('./experiments/real_data/nmf_scaled.csv')
 
         print('\n\nDone with %s k=%s \n\n' %(name,k))
 
